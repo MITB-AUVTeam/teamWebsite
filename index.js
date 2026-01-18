a6dcd9f9135ca0ead9b5a605491dab0edf5e56c1
ChaturVasireddy <chaturvasireddy@gmail.com>
2026-01-18 12:24:17 +0530

    home page files

---
+++ index.js
@@ -0,0 +1,17 @@
+const about = document.getElementById("about");
+const pill = document.getElementById("pill");
+const contactus = document.getElementById("contactus");
+
+function movePill(loc) {
+    const ax = loc.offsetLeft + loc.offsetWidth / 2;
+    const ay = loc.offsetTop + loc.offsetHeight / 2;
+
+    pill.style.left = ax + "px";
+    pill.style.top = ay + "px";
+    pill.style.width = loc.offsetWidth + 50 + "px";
+
+    pill.style.transform = "translate(-50%, -50%)";
+}
+
+movePill(contactus);
+window.addEventListener("resize", movePill);
