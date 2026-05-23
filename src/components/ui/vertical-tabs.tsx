67b5483710d13de060da0d7df5aeafaec0fcc6c7
siddharthps2005 <siddarthps20051@gmail.com>
2026-05-24 01:19:33 +0530

    duration chbge

--- src/components/ui/vertical-tabs.tsx
+++ src/components/ui/vertical-tabs.tsx
@@ -150,7 +150,7 @@ export default function VerticalTabs() {
                             animate={{ opacity: 1, height: "auto" }}
                             exit={{ opacity: 0, height: 0 }}
                             transition={{
-                              duration: 0.4,
+                              duration: 2,
                               ease: [0.23, 1, 0.32, 1],
                             }}
                             className="overflow-hidden"
